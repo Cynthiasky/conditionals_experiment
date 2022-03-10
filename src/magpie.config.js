b7121c0c4d0ec5cb425e1bf11899abb7d5d2c1ba
Cynthia <Cynthia.sky98@gmail.com>
2022-03-10 14:55:34 +0100

    update config

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '171',
-  serverUrl: 'https://magpie-demo.herokuapp.com/',
+  experimentId: '83',//default 171
+  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/api/submit_experiment/',//'https://magpie-demo.herokuapp.com/',
   socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://...',
   contactEmail: 't.li.17@student.rug.nl',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug'
-  //mode: 'directLink'
+  //mode: 'debug'
+  mode: 'directLink'
 };
