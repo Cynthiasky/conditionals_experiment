35d8e0b42d7d67c5c625bd749017d5fb66c3a574
brittaGrusdt <42784325+brittaGrusdt@users.noreply.github.com>
2022-04-25 17:31:58 +0200

    updated prolific completion url

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -4,7 +4,7 @@ export default {
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
   //socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/socket',
   // this will be used in prolific mode
-  completionUrl: '',//'https://...',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=3009C06D', //'https://...',
   contactEmail: 't.li.17@student.rug.nl',
   // Either 'debug', 'directLink' or 'prolific'
   //mode: 'debug'
